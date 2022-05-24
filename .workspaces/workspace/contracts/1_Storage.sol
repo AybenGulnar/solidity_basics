5f1578d59cef91811e5bff2b7b908b15ffe5ede3
DESKTOP-4C73P7U\ayben <ayben.glnar@gmail.com>
2022-05-24 18:13:48 +0300

    first part

---
+++ .workspaces/workspace/contracts/1_Storage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint storedData;
+
+
+    function set(uint x) public {
+        
+        storedData=x;
+    }
+
+    function get() public returns (uint){
+    
+    return storedData;
+    }
+    
+}
